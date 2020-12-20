368792cc190b19c069b24754fae6e197f055f0e6
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-12-20 18:22:30 +0530

    form to add more feeds, error handling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,48 @@
 import styled from "styled-components";
+import { useRef, useState } from "react";
+import Form from "../components/Form";
 import LinkContainer from "../containers/LinkContainer";
+import { fetchFeed } from "../services";
+import { useDispatch, useStore } from "../context";
 
 const HomePage = () => {
+  const ref = useRef("");
+  const { feeds } = useStore();
+  const dispatch = useDispatch();
+  const [error, setError] = useState("");
+
+  const MEDIUM_URL = "https://medium.com/feed/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const refValue = ref.current.value;
+    if (refValue !== "") {
+      const response = await fetchFeed(`${MEDIUM_URL}${refValue}`);
+
+      if (response.status === "ok") {
+        const feedObject = {
+          name: refValue,
+          url: `${MEDIUM_URL}${refValue}`,
+        };
+        const newFeeds = feeds.concat(feedObject);
+
+        dispatch({ type: "FEEDS", payload: newFeeds });
+      } else {
+        setError(response.message);
+      }
+    }
+  };
+
   return (
     <Wrapper>
       <Heading>The place to read your feeds.</Heading>
       <LinkContainer />
+      <Form
+        inputRef={ref}
+        handleSubmit={handleSubmit}
+        MEDIUM_URL={MEDIUM_URL}
+      />
+      {error ? <p>Error - {error}</p> : null}
     </Wrapper>
   );
 };
