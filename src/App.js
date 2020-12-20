58d3bc568b15abc0328ebee0703d0d53ed075226
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-12-20 09:22:29 +0530

    basic rss-json fetch implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { fetchFeed } from "./services";
+import { useState } from "react";
+
 function App() {
-  return <h1>RSS Reader</h1>;
+  return (
+    <>
+      <h1>RSS Reader</h1>
+      <button onClick={() => fetchFeed("https://medium.com/feed/backchannel")}>
+        Fetch Backchannel
+      </button>
+    </>
+  );
 }
 
 export default App;
